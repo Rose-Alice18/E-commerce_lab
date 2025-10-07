7b0dc22d86ccea6acd84ab7faafc22e6aeba9953
Rose-Alice18 <roselinetsatsu@gmail.com>
2025-10-07 15:34:23 +0000

    core updated  session added

--- login/logout.php
+++ login/logout.php
@@ -1,4 +1,27 @@
 <?php
+/**
+ * Logout Script
+ * Destroys user session and redirects to index page
+ */
 
-header('Location: ../login/login.php');
+// Start the session
+session_start();
 
+// Unset all session variables
+$_SESSION = array();
+
+// If you want to destroy the session cookie as well
+if (isset($_COOKIE[session_name()])) {
+    setcookie(session_name(), '', time()-42000, '/');
+}
+
+// Destroy the session
+session_destroy();
+
+// Clear the session completely
+session_write_close();
+
+// Redirect to index page
+header("Location: ../index.php");
+exit();
+?>
